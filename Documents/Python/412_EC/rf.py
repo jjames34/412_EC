66cc21c8b59a6312527a406ac67547e858273e41
vjaros2 <vjaros2@illinois.edu>
2018-04-05 16:07:13 -0500

    Got it in a good format

--- Documents/Python/412_EC/rf.py
+++ Documents/Python/412_EC/rf.py
@@ -9,11 +9,15 @@ filename = "training.csv"
 train_data = np.genfromtxt(filename, skip_header = 1, delimiter = ',', converters = {2: letter_conv},
                             missing_values = '', filling_values = float(0.0), dtype = float).T
 filename = "testing.csv"
-test_x = np.genfromtxt(filename, skip_header = 1, delimiter = ',', converters = {2: letter_conv},
-                            missing_values = '', filling_values = float(0.0), dtype = float)
+test_data = np.genfromtxt(filename, skip_header = 1, delimiter = ',', converters = {2: letter_conv},
+                            missing_values = '', filling_values = float(0.0), dtype = float).T
 
-train_x = train_data[0:-1].T
+
+train_x = train_data[1:-1].T
 train_y = train_data[-1]
+test_x = test_data[1:].T
+test_ids = test_data[0]
+
 
 
 print(train_x.shape, train_y.shape)
@@ -22,6 +26,14 @@ rfc = RandomForestClassifier(random_state=69247)
 rfc.fit(train_x, train_y)
 
 results = rfc.predict(test_x)
+
+f = open('results.csv', 'w')
+f.write('Id,Response\n')
+for i in range(len(results)):
+	line = (int(test_ids[i]), ',', int(results[i]), '\n')
+	for i in line:
+		f.write(str(i))
+f.close()
 
 tally = Counter(results)
 print(tally)
